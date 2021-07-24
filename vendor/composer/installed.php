6cfe2685533d04429a538facd5646a66914a1a67
Ajenkris Yanto Kungkung <kristt26@gmail.com>
2021-07-24 19:36:21 +0900

    library

---
+++ vendor/composer/installed.php
@@ -0,0 +1,44 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9db32d4204d27b962933b2856bfb4ec3ebe13cef',
+    'name' => 'admin/sistempemilihan',
+  ),
+  'versions' => 
+  array (
+    'admin/sistempemilihan' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9db32d4204d27b962933b2856bfb4ec3ebe13cef',
+    ),
+    'ocs/wplib' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '5517fa7e4fa4884e51740d42f781e7d18ee31de1',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '620c2a66c52f4f83746b736a8d1398f50376aa1f',
+    ),
+  ),
+);
